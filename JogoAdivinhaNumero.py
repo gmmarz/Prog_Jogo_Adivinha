7f2c0a37e821f5a64029428ebcae242b54ee1cea
Guilherme Marzano <guilhermemarzano@gmail.com>
2023-10-19 00:08:19 -0300

    First commit - Enunciado

---
+++ JogoAdivinhaNumero.py
@@ -0,0 +1,4 @@
+#Aluno: Guilherme Melo Marzano
+#Objetivo: Jogo de adivinhação, onde pede um número e em seguida pede para usuário tente adivinhar
+#Dando dicas se o número digitado é maior ou menor que o número do segredo.
+
